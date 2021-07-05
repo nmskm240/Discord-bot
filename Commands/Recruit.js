bcef3e14cafad9da193d43a1ecb49df202af21d4
nmskm240 <bokaronogod@gmail.com>
2021-07-05 23:12:41 +0900

    Recruitコマンドのリファクタリング

--- Commands/Recruit.js
+++ Commands/Recruit.js
@@ -1,6 +1,7 @@
 const discord = require("discord.js");
 const Command = require("./Command");
 const Team = require("../Utils/Team");
+const Form = require("../Utils/Form");
 
 exports.modules = class Recruit extends Command {
     constructor() {
@@ -11,16 +12,11 @@ exports.modules = class Recruit extends Command {
             "・募集人数：[省略可]募集する人数を指定する。省略時は人数指定なしとして扱う。参加者が募集人数に達すると募集を終了する。\n" +
             "・募集期間：[省略可]募集を終了するまでの日数や時間を指定する。省略時は1日。\n" +
             "　　2d12h と入力すると、募集開始から2日と12時間後に募集を終了する。");
-        this.size = -1;
-        this.termHour = 0;
-        this.termDate = 1;
-        this.participation = "✅";
-        this.cancel = "❎";
-        this.end = "✖";
-        this.reactionFilter = (reaction, user) =>
-                reaction.emoji.name === this.participation ||
-                reaction.emoji.name === this.cancel ||
-                reaction.emoji.name === this.end;
+        this.reactions = {
+            allow: "✅",
+            cancel: "❎",
+            end: "✖",
+        };
     }
 
     execute(message, parameters) {
@@ -28,111 +24,80 @@ exports.modules = class Recruit extends Command {
             message.channel.send("コマンド引数が足りません。\n");
             return;
         }
+        const parameter = this.parseParameter(parameters);
+        const form = new Form(parameter.size);
+        const embed = form.create("募集中", parameter.formBody, "参加者", message.author);
+        message.channel.send(embed)
+            .then(m => m.react(this.reactions.allow))
+            .then(mReaction => mReaction.message.react(this.reactions.cancel))
+            .then(mReaction => mReaction.message.react(this.reactions.end))
+            .then(mReaction => form.open(mReaction.message, this.reactions, parameter.time.term));
+    }
+
+    parseParameter(parameters) {
+        const parameter = {
+            formBody: "**" + parameters[0] + "**\n\n",
+            time: this.calLimit(),
+            size: -1,
+            term: {
+                date: 0,
+                hour: 0,
+            }
+        }
         const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-        let limit;
         let size = "制限なし";
-        this.participant = new Team("参加者");
-        this.planner = message.author;
         if (3 <= parameters.length) {
-            limit = this.calLimit(parameters[1]);
+            parameter.time = this.calLimit(parameters[1]);
             let parsed = parseInt(parameters[2], 10);
             if (!isNaN(parsed) && 0 < parsed) {
                 size = parsed + "人";
-                this.size = parsed;
+                parameter.size = parsed;
             }
         }
         else if (2 == parameters.length) {
             if (parameters[1].indexOf("d") == -1 && parameters[1].indexOf("h") == -1) {
-                limit = this.calLimit();
                 let parsed = parseInt(parameters[1], 10);
                 if (!isNaN(parsed) && 0 < parsed) {
                     size = parsed + "人";
-                    this.size = parsed;
+                    parameter.size = parsed;
                 }
             }
             else {
-                limit = this.calLimit(parameters[1]);
+                parameter.time = this.calLimit(parameters[1]);
             }
         }
-        else {
-            limit = this.calLimit();
-        }
-        const embed = new discord.MessageEmbed()
-            .setTitle("募集中")
-            .setDescription("**" + parameters[0] + "**\n\n" +
-                this.participation + "：参加、" + this.cancel + "：参加取消\n" +
-                "募集人数：" + size + "\n" +
-                "募集終了：" + limit.toLocaleDateString('ja-JP-u-ca-japanese', options) + "　" +
-                limit.toLocaleTimeString("jp-JP", { hour: '2-digit', minute: '2-digit' }))
-            .setColor("#00a2ff")
-            .addField(this.participant.name, "なし")
-        message.channel.send(embed)
-            .then(m => m.react(this.participation))
-            .then(mReaction => mReaction.message.react(this.cancel))
-            .then(mReaction => mReaction.message.react(this.end))
-            .then(mReaction => {
-                const collector = mReaction.message
-                    .createReactionCollector(this.reactionFilter, {
-                        time: (this.termDate * 24 * 60 * 60 * 1000) + (this.termHour * 60 * 60 * 1000)
-                    });
-                collector.on("collect", (reaction, user) => {
-                    if (reaction.emoji.name === this.participation) {
-                        this.participant.addMember(user);
-                        if (this.participant.members.length == this.size) {
-                            collector.stop();
-                        }
-                    }
-                    else if (reaction.emoji.name === this.cancel) {
-                        this.participant.removeMember(user);
-                        const userReactions = reaction.message.reactions.cache.filter(reaction =>
-                            reaction.users.cache.has(user.id) &&
-                            (reaction.emoji.name === this.participation ||
-                                reaction.emoji.name === this.cancel));
-                        try {
-                            for (const reaction of userReactions.values()) {
-                                reaction.users.remove(user.id);
-                            }
-                        } catch (error) {
-                            console.error('Failed to remove reactions.');
-                        }
-                    }
-                    else {
-                        if (user.id == this.planner.id) {
-                            collector.stop();
-                        }
-                    }
-                    let embedField = Object.assign({}, embed.fields[0]);
-                    embedField.value = this.participant.members.length == 0 ? "なし" : this.participant.members;
-                    reaction.message.embeds[0].fields[0] = embedField;
-                    reaction.message.edit(new discord.MessageEmbed(reaction.message.embeds[0]));
-                });
-                collector.on("end", collection => {
-                    mReaction.message.embeds[0].title = "募集終了";
-                    mReaction.message.embeds[0].color = "#000000";
-                    mReaction.message.edit(new discord.MessageEmbed(mReaction.message.embeds[0]));
-                });
-            });
+        parameter.formBody += this.reactions.allow + "：参加、" + this.reactions.cancel + "：参加取消\n" +
+            "募集人数：" + size + "\n" +
+            "募集終了：" + parameter.time.limit.toLocaleDateString('ja-JP-u-ca-japanese', options) + "　" +
+            parameter.time.limit.toLocaleTimeString("jp-JP", { hour: '2-digit', minute: '2-digit' })
+        return parameter;
     }
 
     calLimit(input = "1d") {
-        let limit = new Date();
+        const data = {
+            limit: new Date(),
+            term: {
+                date: 1,
+                hour: 0,
+            }
+        }
         const dIndex = input.indexOf("d");
         const hIndex = input.indexOf("h");
         if (dIndex != -1) {
             let parsed = parseInt(input.substring(0, dIndex), 10);
             if (!isNaN(parsed) && 0 < parsed) {
-                limit.setDate(limit.getDate() + parsed);
+                data.limit.setDate(data.limit.getDate() + parsed);
+                data.term.date = parsed;
             }
         }
         if (hIndex != -1) {
             let parsed = parseInt(input.substring(dIndex, hIndex), 10);
             if (!isNaN(parsed) && 0 < parsed) {
-                limit.setHours(limit.getHours() + parsed);
+                data.limit.setHours(data.limit.getHours() + parsed);
+                data.term.hour = parsed;
             }
         }
-        limit.setHours(limit.getHours() + 9);
-        this.termDate = limit.getDate();
-        this.termHour = limit.getHours();
-        return limit;
+        data.limit.setHours(data.limit.getHours() + 9);
+        return data;
     }
 }
