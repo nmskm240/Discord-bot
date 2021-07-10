3a8b126bd9bf5b6f4e08a17162017c9f7966aa31
nmskm240 <bokaronogod@gmail.com>
2021-07-10 19:08:12 +0900

    Networkエラー時にログを出力するように変更

--- Utils/Network.js
+++ Utils/Network.js
@@ -1,33 +1,41 @@
 const axiosBase = require("axios");
-const url = 
+const baseURL = "https://script.google.com";
 
 module.exports = class Network {
-    static get(query){
+    static async get(query) {
         const axios = axiosBase.create({
-            baseURL: "https://script.google.com",
+            baseURL: baseURL,
         });
         const res = await axios.get("/macros/s/AKfycbzIR3Pyzlpc8sfBQ1B6a8v5SOA1Y2GjEijwzZttIZTyQSJYnD5P8V9DHcLFPU_0YBAF/exec", {
             params: query,
         })
+            .catch(error => {
+                console.log("GASへのGETに失敗");
+                return error.response;
+            });
+        if (res.status != 200) {
+            return null;
+        }
         return res;
     }
 
-    static post(sendData) {
+    static async post(sendData) {
         const axios = axiosBase.create({
-            baseURL: "https://script.google.com",
+            baseURL: baseURL,
             headers: {
                 "Content-Type": "application/json",
                 "X-Requested-With": "XMLHttpRequest",
             },
             responseType: "json",
         });
-        axios.post("/macros/s/AKfycbzIR3Pyzlpc8sfBQ1B6a8v5SOA1Y2GjEijwzZttIZTyQSJYnD5P8V9DHcLFPU_0YBAF/exec", sendData)
-            .then(async function (response) {
-                console.log(response.data);
-            })
-            .catch(function (error) {
-                console.log("ERROR!! occurred in Backend.");
-                console.error(error);
+        const res = await axios.post("/macros/s/AKfycbzIR3Pyzlpc8sfBQ1B6a8v5SOA1Y2GjEijwzZttIZTyQSJYnD5P8V9DHcLFPU_0YBAF/exec", sendData)
+            .catch(error => {
+                console.log("GASへのPOSTに失敗");
+                return error.response;
             });
+        if (res.status != 200) {
+            return null;
+        }
+
     }
 }
