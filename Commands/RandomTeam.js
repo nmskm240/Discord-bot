74a526f9d49c6efab3b3151fd7b6f9432684ae5a
nmskm240 <bokaronogod@gmail.com>
2021-07-31 15:11:06 +0900

    ランク帯などを考慮してチーム分けができるように変更

--- Commands/RandomTeam.js
+++ Commands/RandomTeam.js
@@ -5,18 +5,54 @@ const Command = require("./Command");
 
 module.exports = class RandomTeam extends Command {
     execute(message, parameters) {
-        const parameter = this.parseParameter(message, parameters);
+        const parameter = this.parseParameter(message, parameters.filter(parameter => !parameter.match(/^<@!/)));
         const embed = new discord.MessageEmbed()
             .setTitle("チーム分け結果");
-        this.make(parameter.members, parameter.size).forEach(team => {
-            embed.addField(team.name, team.members);
-        });
-        message.channel.send(embed);
-        return embed;
+        this.make(parameter.members, parameter.size)
+            .then(teams => {
+                teams.forEach(team => {
+                    embed.addField(team.name, team.members);
+                });
+                message.channel.send(embed);
+            });
     }
 
-    make(members, size) {
-        return utils.Team.random(members, size);
+    async make(members, size) {
+        let teams;
+        if (!this.game) {
+            teams = utils.Team.random(members, size);
+        }
+        else {
+            const count = Math.floor(members.length / size);
+            try {
+                await utils.Network.get({ command: "rank" })
+                    .then(res => {
+                        const sortedData = res.data.sort((a, b) => b[this.game][this.key] - a[this.game][this.key]);
+                        console.log(sortedData);
+                        let sorted = [];
+                        sortedData.forEach(data => {
+                            members.forEach(member => {
+                                if (member.user.tag.indexOf(data.DiscordTag) != -1) {
+                                    sorted.push(member);
+                                    return;
+                                }
+                            });
+                        });
+                        console.log(sorted);
+                        const top = sorted.splice(0, count);
+                        teams = utils.Team.random(sorted, size - 1);
+                        for (let i = 0; i < top.length; i++) {
+                            teams[i].max++;
+                            teams[i].refresh();
+                            teams[i].addMember(top[i]);
+                        }
+                    })
+            }
+            catch (e) {
+                teams = utils.Team.random(members, size);
+            }
+        }
+        return teams;
     }
 
     parseParameter(message, parameters) {
@@ -24,9 +60,25 @@ module.exports = class RandomTeam extends Command {
             size: 3,
             members: message.mentions.members.array(),
         }
-        const parsed = parseInt(parameters[0]);
-        if (!isNaN(parsed) && 0 < parsed) {
-            parameter.size = parsed;
+        if (0 < parameters.length) {
+            const parsed = parseInt(parameters[0]);
+            if (!isNaN(parsed) && 0 < parsed) {
+                parameter.size = parsed;
+            }
+            else if (isNaN(parsed)) {
+                this.game = parameters[0];
+                this.key = "rank";
+                if (1 < parameters.length) {
+                    this.key = parameters[1];
+                }
+            }
+            if (1 < parameters.length) {
+                this.game = parameters[1];
+                this.key = "rank";
+                if (2 < parameters.length) {
+                    this.data = parameters[2];
+                }
+            }
         }
         return parameter;
     }
--- Commands/rtc.js
+++ Commands/rtc.js
@@ -1,12 +1,13 @@
 const requireDir = require("require-dir");
 const commands = requireDir("../Commands");
-const utils = requireDir("../Utils");
 
 module.exports = class rtc extends commands.RandomTeam {
     constructor() {
         super("rtc",
             "メンションで指定したメンバーでランダムなチームを作成します。\n",
             new commands.Parameter("1チームの人数", "1チームの人数を指定します。", "0以上の整数", false, true, "3"),
+            new commands.Parameter("対象ゲーム", "チーム分けを行うゲームを指定します。", "半角英語大文字でのゲームタイトル（APEX, LOL等）", false, true, "空白文字列"),
+            new commands.Parameter("考慮対象", "チーム分け時に考慮する情報を指定します。", "半角英語子文字（rank, kdr, damage等）", false, true, "rank"),
             new commands.Parameter("対象メンバー", "チーム分けに含めるメンバーを指定します。", "メンション", true));
     }
 }
