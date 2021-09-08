1a78fec1afe9bb8f6c1f8ee1fb8b629b6394cad3
nmskm240 <bokaronogod@gmail.com>
2021-09-08 11:29:01 +0900

    recruitコマンドが動作するように修正

--- src/Commands/recruit.ts
+++ src/Commands/recruit.ts
@@ -1,12 +1,21 @@
 import { Message, MessageEmbed } from "discord.js";
 import { Command } from "./Command";
 import { Form } from "../Utils";
-import { FreeWriteParameter, OmittableNumberParameter } from "./Parameters/";
+import { FreeWriteParameter, OmittableNumberParameter } from "./Parameters";
 
 export class Recruit extends Command {
     private _form: Form = new Form();
 
-    reactions: any;
+    reactions: any = {
+        allow: "✅",
+        cancel: "❎",
+        close: "✖",
+    };
+    term: any = {
+        date: 1,
+        hour: 0,  
+    };
+
     constructor() {
         super(
             "recruit",
@@ -17,25 +26,23 @@ export class Recruit extends Command {
                 // new Parameter("募集期間", "募集を終了するまでの日時を指定します。", "XdYh(X,Yは0以上の整数)", false, true, "1d")
             ]
         );
-        this.reactions = {
-            allow: "✅",
-            cancel: "❎",
-            close: "✖",
-        };
     }
 
     public async execute(): Promise<MessageEmbed> {
-        return this._form.create("募集中", this.parameters[0].valueOrDefault, "参加者", this.info.message!.author);
-        // .then((m: any) => m.react(this.reactions.allow))
-        // .then((mReaction: any) => mReaction.message.react(this.reactions.cancel))
-        // .then((mReaction: any) => mReaction.message.react(this.reactions.close))
-        // .then((mReaction: any) => form.open(mReaction.message, this.reactions, parameter.time.term));
+        const now: Date = new Date();
+        const limit: string = 0 < this.parameters[1].valueOrDefault ? this.parameters[1].valueOrDefault.toString() : "制限なし";
+        now.setDate(now.getDate() + 1);
+        const body:string = this.parameters[0].valueOrDefault + "\n\n" +
+            this.reactions.allow + "：参加 " + this.reactions.cancel + "：参加取消\n" + 
+            "募集人数：" + limit + "\n" + 
+            "募集終了：" + now.toString(); 
+        return this._form.create("募集中", body, "参加者", this.info.message!.author);
     }
 
     public async onComplite(message: Message): Promise<void> {
-        for(const reaction of this.reactions.values) {
+        for (const reaction of Object.values<string>(this.reactions)) {
             await message.react(reaction);
         }
-        
+        this._form.open(message, this.reactions, this.term);
     }
 }
